501abd048e7302d20fd655952daec100a306a3c3
wxb_101 <wxb_101@163.com>
2020-09-13 18:24:26 +0800

    add env&&spi opt

---
+++ src/main/java/ink/zfei/boot/context/properties/EnableConfigurationPropertiesRegistrar.java
@@ -0,0 +1,11 @@
+package ink.zfei.boot.context.properties;
+
+import ink.zfei.summer.context.AnnotationConfigApplicationContext;
+import ink.zfei.summer.core.ImportBeanDefinitionRegistrar;
+
+public class EnableConfigurationPropertiesRegistrar implements ImportBeanDefinitionRegistrar {
+    @Override
+    public void registerBeanDefinitions(AnnotationConfigApplicationContext annotationConfigApplicationContext, Class aClass) {
+
+    }
+}
---
+++ src/main/java/ink/zfei/boot/demo/RedisProties.java
@@ -0,0 +1,14 @@
+package ink.zfei.boot.demo;
+
+public class RedisProties {
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    private String host;
+
+}
